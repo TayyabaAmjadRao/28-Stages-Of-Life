31cf175120bd346cbc82a437ec07c6a87770714b
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:28:25 -0800

    Add files via upload

---
+++ Stages_of_life.js
@@ -0,0 +1,22 @@
+"use strict";
+//Written by : TAYYABA RAO
+//Date : 18/feb/2024
+let age = 25;
+if (age < 2) {
+    console.log("The person is a baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("The person is toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("The person is kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("The person is teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("The peson is an adult.");
+}
+else {
+    console.log("The person is an elder.");
+}
